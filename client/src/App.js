7b974c489937a4c6bfa2dde195d4ee29179c34f6
Raphael <you@example.com>
2022-03-03 18:50:02 +0200

    fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@mui/styles';
+import { useEffect, useState } from 'react';
 
 const useStyles = makeStyles({
     root: {
@@ -8,6 +9,19 @@ const useStyles = makeStyles({
 
 function App() {
     const classes = useStyles()
+
+    const [dogsList, setDogsList] = useState([])
+
+    useEffect(() => {
+        console.log('App -> useEffect')
+
+        fetch('http://localhost:5000/api/dogs')
+            .then(res => res.json())
+            .then(data => {
+                console.log('data')
+                console.log(data)
+            })
+    }, [])
 
     return (
         <div className={classes.root}>
